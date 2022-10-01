07d099118c7e854fc28e5a1c3c59b1f64217f0bd
KT <kt@KTs-MacBook-Air.T-mobile.com>
2022-10-01 16:00:22 -0700

    guess

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [randomButtons, setRandomButtons] = useState([]);
+  const [bgColor, setBgColor] = useState();
+  const [clickedButton, setClickedButton] = useState("");
+  const [correctButton, setCorrectButton] = useState(undefined);
+
+  function randomArr() {
+    let newArr = [];
+    for (let i = 0; i < 3; i++) {
+      newArr.push(randomColor());
+    }
+
+    return newArr;
+  }
+
+  useEffect(() => {
+    const random = randomArr();
+
+    setRandomButtons(random);
+    setBgColor(random[Math.floor(Math.random() * 3)]);
+  }, [clickedButton]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <div
+        style={{ backgroundColor: "#" + bgColor }}
+        className={`w-[300px] h-[300px] mx-auto mt-[100px]`}
+      >
+        {bgColor}
+      </div>
+      <div className="flex justify-center">
+        {randomButtons.map((item) => {
+          return (
+            <Button
+              key={item}
+              {...{
+                bgColor,
+                setBgColor,
+                item,
+                randomArr,
+                clickedButton,
+                setClickedButton,
+                correctButton,
+                setCorrectButton,
+              }}
+            />
+          );
+        })}
+      </div>
+      <div className="mx-auto">
+        {correctButton === undefined ? "" : correctButton ? "correct" : "wrong"}
+      </div>
     </div>
+  );
+}
+
+function randomColor() {
+  let hex = [];
+  const hexLetters = ["A", "B", "C", "D", "E", "F"];
+  const hexNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const arr = hexLetters.concat(hexNumbers);
+
+  for (let i = 0; i < 6; i++) {
+    hex += randomGenerator(arr);
+  }
+
+  return hex.toString();
+}
+
+function randomGenerator(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function Button({
+  item,
+  bgColor,
+  setClickedButton,
+  correctButton,
+  setCorrectButton,
+}) {
+  const color = "#" + randomColor();
+
+  function handleClick(e) {
+    if (e.target.innerHTML === bgColor) {
+      setCorrectButton(true);
+    } else {
+      setCorrectButton(false);
+    }
+    setClickedButton(e.target.innerHTML);
+  }
+
+  return (
+    <button onClick={handleClick} className="p-2 bg-gray-500 m-2">
+      {item}
+    </button>
   );
 }
 
