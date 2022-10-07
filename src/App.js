a90b963ac79e3655e6da06c35bc607736adf7c4c
KT <kt@KTs-MacBook-Air.T-mobile.com>
2022-10-07 12:19:59 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,64 +1,7 @@
-import React, { useEffect, useState } from "react";
-
-const divs = [1, 2, 3, 4, 5];
+import React from "react";
 
 function App() {
-  const [keypress, setKeypress] = useState(null);
-  const [selectedDiv, setSelectedDiv] = useState();
-  const [selectedClass, setSelectedClass] = useState("bg-green-300");
-
-  console.log("kim".charAt(Math.floor(Math.random() * "kim".length)));
-
-  function handleClick(e) {}
-
-  function handleKeydown(e) {
-    switch (e.key) {
-      case "ArrowUp":
-        if (selectedDiv === 1) return;
-        setSelectedDiv((prev) => prev - 1);
-
-        break;
-      case "ArrowDown":
-        if (selectedDiv === 6) return;
-        setSelectedDiv((prev) => prev + 1);
-
-        break;
-      case "Enter":
-        setSelectedDiv(divs.length + 1);
-        break;
-    }
-  }
-  useEffect(() => {
-    setSelectedDiv(divs.length + 1);
-  }, []);
-  useEffect(() => {
-    console.log("useeffect");
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      console.log("remove useeffect");
-
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  }, [handleKeydown]);
-  return (
-    <div className="flex flex-col gap-2 items-center">
-      {divs.map((div) => {
-        return (
-          <div
-            key={div}
-            id={div}
-            onClick={handleClick}
-            className={`w-[200px] h-[50px] rounded ${
-              selectedDiv === div ? selectedClass : "bg-gray-300"
-            } flex items-center justify-center`}
-          >
-            {div}
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
